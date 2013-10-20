91a98331280067815ec6f6f857653c31cacee112
Harihar Subramanyam <hsubrama@mit.edu>
2013-10-20 09:49:24 -0400

    Began header

---
+++ src/player/Header.java
@@ -0,0 +1,9 @@
+package player;
+
+/**
+ * Represents the header of an ABC music file
+ * This is immutable because each of the fields are immutable
+ */
+public class Header {
+    private final 
+}
---
+++ src/player/KeySignature.java
@@ -0,0 +1,8 @@
+package player;
+
+/**
+ * A key signature specifies accidentals that must be applied a set of notes
+ * This class provides an enumeration of all the key signatures and a map to the accidentals they provide 
+ */
+public class KeySignature {
+}
