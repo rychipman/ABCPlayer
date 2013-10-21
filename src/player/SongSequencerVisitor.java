64eaffb5cff56b4e67803c2d4fe286c24da510fb
Harihar Subramanyam <hsubrama@mit.edu>
2013-10-21 19:58:50 -0400

    Implemented lcm

--- src/player/SongSequencerVisitor.java
+++ src/player/SongSequencerVisitor.java
@@ -3,6 +3,8 @@ package player;
 import java.util.HashMap;
 import java.util.List;
 
+import sound.Pitch;
+
 public class SongSequencerVisitor implements ISongSequencerVisitor{
     private KeySignature keySignature;
     private Fraction noteLengthPerBeat;
@@ -36,5 +38,11 @@ public class SongSequencerVisitor implements ISongSequencerVisitor{
     public void visit(Voice voice) {
         this.musicForVoiceName.put(voice.getVoiceName(), voice.getSongComponents());
     }
-
+    
+    public void play(){
+        Fraction beatMeasure = new Fraction(1,1);
+        for (String voiceName : this.musicForVoiceName.keySet())
+            for(Music m : this.musicForVoiceName.get(voiceName))
+                beatMeasure = new Fraction(1, Fraction.LCM(m.getDuration().getDenominator(), beatMeasure.getDenominator()));
+    }
 }
