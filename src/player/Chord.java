be1459f077df5b608536e2baf8b57c15ee5b3326
Harihar Subramanyam <hsubrama@mit.edu>
2013-10-21 12:56:36 -0400

    Modifying chord

---
+++ src/player/Chord.java
@@ -0,0 +1,37 @@
+package player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chord implements Music{
+
+    private final List<Note> notes;
+    
+    public Chord(List<Note> notes){
+        assert notes.size() > 1;
+        this.notes = new ArrayList<Note>();
+        for (Note n : notes)
+            this.notes.add(n);
+    }
+    
+    @Override
+    public void accept(ISongSequencerVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    @Override
+    public Music copy() {
+        return new Chord(this.notes);
+    }
+
+    @Override
+    public Fraction getDuration() {
+        Fraction maxDuration = new Fraction(0,1);
+        for (Note n : this.notes)
+            if (n.getDuration().compareTo(maxDuration) > 0)
+                maxDuration = n.getDuration();
+        return maxDuration;
+        
+    }
+
+}
