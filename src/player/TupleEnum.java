de8643c5d948f7511fb7c868736b281406dc1d38
Harihar Subramanyam <hsubrama@mit.edu>
2013-10-21 02:18:40 -0400

    toString for TupleNum

--- src/player/TupleEnum.java
+++ src/player/TupleEnum.java
@@ -2,4 +2,18 @@ package player;
 
 public enum TupleEnum {
     DUPLET, TRIPLET, QUADRUPLET;
+    
+    @Override
+    public String toString(){
+        switch(this){
+        case DUPLET:
+            return "(2";
+        case TRIPLET:
+            return "(3";
+        case QUADRUPLET:
+            return "(4";
+        default:
+            return "X";
+        }
+    }
 }
