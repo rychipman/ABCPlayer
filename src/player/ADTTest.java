65445d7264e587431555b8e9cc3fa9dbcb924f75
Harihar Subramanyam <hsubrama@mit.edu>
2013-10-21 22:30:41 -0400

    Added more notes to test

--- src/player/ADTTest.java
+++ src/player/ADTTest.java
@@ -34,11 +34,31 @@ public class ADTTest {
         music.add(new Note(NoteEnum.D, AccidentalEnum.NATURAL, 2, new Fraction(1,8)));
         music.add(new Note(NoteEnum.D, AccidentalEnum.NATURAL, 2, new Fraction(2,4)));
         
+        music.add(new Note(NoteEnum.E, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.E, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.F, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.G, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        
+        music.add(new Note(NoteEnum.G, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.F, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.E, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.D, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        
+        music.add(new Note(NoteEnum.C, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.C, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.D, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        music.add(new Note(NoteEnum.E, AccidentalEnum.NATURAL, 2, new Fraction(1,4)));
+        
+        music.add(new Note(NoteEnum.D, AccidentalEnum.NATURAL, 2, new Fraction(3,8)));
+        music.add(new Note(NoteEnum.C, AccidentalEnum.NATURAL, 2, new Fraction(1,8)));
+        music.add(new Note(NoteEnum.C, AccidentalEnum.NATURAL, 2, new Fraction(2,4)));
+        
+        
         Voice voice = new Voice("Test Voice", music);
         List<Voice> voices = new ArrayList<Voice>();
         voices.add(voice);
         Body body = new Body(voices);
-        Header header = new Header(3, "Test Title", "Test Composer", new KeySignature("A"), new Fraction(1,4), 30);
+        Header header = new Header(3, "Test Title", "Test Composer", new KeySignature("A"), new Fraction(1,4), 60);
         Song song = new Song(header, body);
         SongSequencerVisitor visitor = new SongSequencerVisitor();
         song.accept(visitor);
