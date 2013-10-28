b2cdb3d446ec76de655756e5967334d6cba5a2ce
Danielle Man <daniman0808@gmail.com>
2013-10-28 01:41:57 -0400

    Extended Lexer tests

--- src/grammar/ABCMusicLexerTest.java
+++ src/grammar/ABCMusicLexerTest.java
@@ -19,42 +19,42 @@ public class ABCMusicLexerTest {
 	@Test
 	public void testDigits() {
 		String input = "0123456789";
-		String[] expected = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		String[] expected = {"0123456789"};
 		verifyLexer(input, expected);
 	}
 	
 	@Test
-	public void testNotes() {
+	public void testBaseNotes() {
 		String input = "C D E F G A B c d e f g a b";
 		String[] expected = {"C", " ", "D", " ", "E", " ", "F", " ", "G", " ", "A", " ", "B", " ", "c", " ", "d", " ", "e", " ", "f", " ", "g", " ", "a", " ", "b"};
 		verifyLexer(input, expected);
 	}
 	
 	@Test
-	public void testKeyAccidentals() {
+	public void testAccidentals() {
 		String input = "C#Bbab";
-		String[] expected = {"C", "#", "B", "b", "a", "b"};
+		String[] expected = {"C#", "Bb", "ab"};
 		verifyLexer(input, expected);
 	}
 	
 	@Test
 	public void testMode() {
-		String input = "Gbm";
-		String[] expected = {"G", "b", "m"};
+		String input = "Gm b";
+		String[] expected = {"Gm", " ", "b"};
 		verifyLexer(input, expected);
 	}
 	
 	@Test
 	public void testSpace() {
 		String input = "A  C   bb";
-		String[] expected = {"A", " ", " ", "C", " ", " ", " ", "b", "b"};
+		String[] expected = {"A", " ", " ", "C", " ", " ", " ", "bb"};
 		verifyLexer(input, expected);
 	}
 	
 	@Test
 	public void testRests() {
 		String input = "ab z z C";
-		String[] expected = {"a", "b", " ", "z", " ", "z", " ", "C"};
+		String[] expected = {"ab", " ", "z", " ", "z", " ", "C"};
 		verifyLexer(input, expected);
 	}
 	
@@ -78,6 +78,42 @@ public class ABCMusicLexerTest {
 		String[] expected = {"|", " ", "||", " ", "[|", " ", "|]", " ", "|:", " ", ":|", "|"};
 		verifyLexer(input, expected);
 	}
+	
+	@Test
+	public void testFractions() {
+	    String input = "3/4 1/2 2C";
+	    String[] expected = {"3/4", " ", "1/2", " ", "2", "C"};
+	    verifyLexer(input, expected);
+	}
+	
+	@Test
+    public void testKey() {
+        String input = "Gbm";
+        String[] expected = {"Gbm"};
+        verifyLexer(input, expected);
+    }
+	
+	@Test
+    public void testTempo() {
+        String input = "42 1/4=234C";
+        String[] expected = {"42", " ", "1/4=234", "C"};
+        verifyLexer(input, expected);
+    }
+	
+	@Test
+    public void testMeter() {
+        String input = "CC| 4/4";
+        String[] expected = {"C", "C|", " ", "4/4"};
+        verifyLexer(input, expected);
+    }
+	
+	@Test
+    public void testBrackets() {
+        String input = "[B1/2 G]";
+        String[] expected = {"[", "B", "1/2", " ", "G", "]"};
+        verifyLexer(input, expected);
+    }
+	
 	
 //	@Test(expected=RuntimeException.class)
 //	public void testExceptions() {
