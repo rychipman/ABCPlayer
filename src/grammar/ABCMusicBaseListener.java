63455eadb181be1ea6167435e25b52a2eb8a7614
Ryan Chipman <rychipman@gmail.com>
2013-10-28 01:15:14 -0400

    organized listener methods

--- src/grammar/ABCMusicBaseListener.java
+++ src/grammar/ABCMusicBaseListener.java
@@ -8,69 +8,84 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.ErrorNode;
 
+import player.Song;
+
 public class ABCMusicBaseListener implements ABCMusicListener {
+	private Song song;
+	/**
+	 * Top-level elements
+	 */
+	@Override public void enterAbc_tune(ABCMusicParser.Abc_tuneContext ctx) { }
+	@Override public void exitAbc_tune(ABCMusicParser.Abc_tuneContext ctx) { }
+	
+	@Override public void enterAbc_header(ABCMusicParser.Abc_headerContext ctx) { }
+	@Override public void exitAbc_header(ABCMusicParser.Abc_headerContext ctx) { }
+	
+	@Override public void enterAbc_music(ABCMusicParser.Abc_musicContext ctx) {	}
+	@Override public void exitAbc_music(ABCMusicParser.Abc_musicContext ctx) { }
+	
+	@Override public void enterEnd_of_line(ABCMusicParser.End_of_lineContext ctx) { }
+	@Override public void exitEnd_of_line(ABCMusicParser.End_of_lineContext ctx) { }
+	
+	/**
+	 * Header Elements
+	 */
+
+	@Override public void enterField_number(ABCMusicParser.Field_numberContext ctx) { }
+	@Override public void exitField_number(ABCMusicParser.Field_numberContext ctx) { }
+
+	@Override public void enterField_title(ABCMusicParser.Field_titleContext ctx) { }
+	@Override public void exitField_title(ABCMusicParser.Field_titleContext ctx) { }
+
+	@Override public void enterField_composer(ABCMusicParser.Field_composerContext ctx) { }
+	@Override public void exitField_composer(ABCMusicParser.Field_composerContext ctx) { }
+
+	@Override public void enterField_meter(ABCMusicParser.Field_meterContext ctx) { }
+	@Override public void exitField_meter(ABCMusicParser.Field_meterContext ctx) { }
+
+	@Override public void enterField_default_length(ABCMusicParser.Field_default_lengthContext ctx) { }
+	@Override public void exitField_default_length(ABCMusicParser.Field_default_lengthContext ctx) { }
+
 	@Override public void enterField_tempo(ABCMusicParser.Field_tempoContext ctx) { }
 	@Override public void exitField_tempo(ABCMusicParser.Field_tempoContext ctx) { }
 
+	@Override public void enterField_key(ABCMusicParser.Field_keyContext ctx) { }
+	@Override public void exitField_key(ABCMusicParser.Field_keyContext ctx) { }
+
+	@Override public void enterOther_fields(ABCMusicParser.Other_fieldsContext ctx) { }
+	@Override public void exitOther_fields(ABCMusicParser.Other_fieldsContext ctx) { }
+	
+	@Override public void enterField_voice(ABCMusicParser.Field_voiceContext ctx) { }
+	@Override public void exitField_voice(ABCMusicParser.Field_voiceContext ctx) { }
+	
+	/**
+	 * Music Elements
+	 */
+
+	@Override public void enterAbc_line(ABCMusicParser.Abc_lineContext ctx) { }
+	@Override public void exitAbc_line(ABCMusicParser.Abc_lineContext ctx) { }
+
+	@Override public void enterMid_tune_field(ABCMusicParser.Mid_tune_fieldContext ctx) { }
+	@Override public void exitMid_tune_field(ABCMusicParser.Mid_tune_fieldContext ctx) { }
+	
 	@Override public void enterElement(ABCMusicParser.ElementContext ctx) { }
 	@Override public void exitElement(ABCMusicParser.ElementContext ctx) { }
 
 	@Override public void enterMultinote(ABCMusicParser.MultinoteContext ctx) { }
 	@Override public void exitMultinote(ABCMusicParser.MultinoteContext ctx) { }
 
-	@Override public void enterMid_tune_field(ABCMusicParser.Mid_tune_fieldContext ctx) { }
-	@Override public void exitMid_tune_field(ABCMusicParser.Mid_tune_fieldContext ctx) { }
-
-	@Override public void enterAbc_music(ABCMusicParser.Abc_musicContext ctx) { }
-	@Override public void exitAbc_music(ABCMusicParser.Abc_musicContext ctx) { }
-
-	@Override public void enterOther_fields(ABCMusicParser.Other_fieldsContext ctx) { }
-	@Override public void exitOther_fields(ABCMusicParser.Other_fieldsContext ctx) { }
-
-	@Override public void enterAbc_header(ABCMusicParser.Abc_headerContext ctx) { }
-	@Override public void exitAbc_header(ABCMusicParser.Abc_headerContext ctx) { }
-
-	@Override public void enterEnd_of_line(ABCMusicParser.End_of_lineContext ctx) { }
-	@Override public void exitEnd_of_line(ABCMusicParser.End_of_lineContext ctx) { }
+	@Override public void enterTuplet_element(ABCMusicParser.Tuplet_elementContext ctx) { }
+	@Override public void exitTuplet_element(ABCMusicParser.Tuplet_elementContext ctx) { }
 
 	@Override public void enterNote_element(ABCMusicParser.Note_elementContext ctx) { }
 	@Override public void exitNote_element(ABCMusicParser.Note_elementContext ctx) { }
 
-	@Override public void enterAbc_line(ABCMusicParser.Abc_lineContext ctx) { }
-	@Override public void exitAbc_line(ABCMusicParser.Abc_lineContext ctx) { }
-
 	@Override public void enterNote_length(ABCMusicParser.Note_lengthContext ctx) { }
 	@Override public void exitNote_length(ABCMusicParser.Note_lengthContext ctx) { }
-
-	@Override public void enterField_composer(ABCMusicParser.Field_composerContext ctx) { }
-	@Override public void exitField_composer(ABCMusicParser.Field_composerContext ctx) { }
-
-	@Override public void enterAbc_tune(ABCMusicParser.Abc_tuneContext ctx) { }
-	@Override public void exitAbc_tune(ABCMusicParser.Abc_tuneContext ctx) { }
-
-	@Override public void enterField_key(ABCMusicParser.Field_keyContext ctx) { }
-	@Override public void exitField_key(ABCMusicParser.Field_keyContext ctx) { }
-
-	@Override public void enterField_default_length(ABCMusicParser.Field_default_lengthContext ctx) { }
-	@Override public void exitField_default_length(ABCMusicParser.Field_default_lengthContext ctx) { }
-
-	@Override public void enterField_meter(ABCMusicParser.Field_meterContext ctx) { }
-	@Override public void exitField_meter(ABCMusicParser.Field_meterContext ctx) { }
-
-	@Override public void enterField_number(ABCMusicParser.Field_numberContext ctx) { }
-	@Override public void exitField_number(ABCMusicParser.Field_numberContext ctx) { }
-
-	@Override public void enterTuplet_element(ABCMusicParser.Tuplet_elementContext ctx) { }
-	@Override public void exitTuplet_element(ABCMusicParser.Tuplet_elementContext ctx) { }
-
-	@Override public void enterField_title(ABCMusicParser.Field_titleContext ctx) { }
-	@Override public void exitField_title(ABCMusicParser.Field_titleContext ctx) { }
 
 	@Override public void enterPitch(ABCMusicParser.PitchContext ctx) { }
 	@Override public void exitPitch(ABCMusicParser.PitchContext ctx) { }
 
-	@Override public void enterField_voice(ABCMusicParser.Field_voiceContext ctx) { }
-	@Override public void exitField_voice(ABCMusicParser.Field_voiceContext ctx) { }
 
 	@Override public void enterEveryRule(ParserRuleContext ctx) { }
 	@Override public void exitEveryRule(ParserRuleContext ctx) { }
